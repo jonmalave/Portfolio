9e22d12c3883ab5ccb0f60abec13ce368bc485e0
Jon Malave <jonmalave@gmail.com>
2019-03-07 01:55:53 -0500

    Add metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,26 @@ module.exports = {
       { name: 'viewport', content: 'minimal-ui, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { charset: 'utf-8' },
       { hid: 'robots', name: 'robots', content: 'index, follow' },
-      { hid: 'description', name: 'description', content: 'Jon Malave | Web Developer' },
-      { hid: 'keywords', name: 'keywords', content: 'nuxt, portfolio, web developer' }  
+      { hid: 'description', name: 'description', content: 'Web developer with a passion for responsive web design, and single-page application development.' },
+      { hid: 'keywords', name: 'keywords', content: 'vue.js, nuxt.js, web, developer, miami' },
+      { hid: 'og:locale', property: 'og:locale', content: 'en' },
+      { hid: 'og:title', property: 'og:title', content: 'Jon Malave' },
+      { hid: 'og:description', property: 'og:description', content: 'Web developer with a passion for responsive web design, and single-page application development.' },
+      { hid: 'og:type', property: 'og:type', content: 'article' },
+      { hid: 'og:url', property: 'og:url', content: 'https://jonmalave.com' },
+      { hid: 'og:image', property: 'og:image', content: 'https://jonmalave.com/img/thumbnail.jpg' },
+      { hid: 'og:image:width', property: 'og:image:width', content: '1024' },
+      { hid: 'og:image:height', property: 'og:image:height', content: '512' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'jonmalave.com' },
+      { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:site', property: 'twitter:site', content: '@jonmalave' },
+      { hid: 'twitter:creator', property: 'twitter:creator', content: 'Jon Malave' },
+      { hid: 'twitter:title', property: 'twitter:title', content: 'Jon Malave' },
+      { hid: 'twitter:description', property: 'twitter:description', content: 'Web developer with a passion for responsive web design, and single-page application development.' },
+      { hid: 'twitter:image', property: 'twitter:image', content: 'https://jonmalave.com/img/thumbnail.jpg' }      
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'https://jonmalave.com/img/icon.jpg' }
     ] 
   },
 
