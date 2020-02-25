f6382c59a57fce0b40dd4914ce3cdfd3f238cbd1
Jon Malave <jonmalave@gmail.com>
2020-02-24 20:40:23 -0500

    Update about

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
         Jon Malave
       </p>
       <p class="content__article-subtitle">
-        Full-Stack Web Developer based in Miami.
+        Web Developer based in Miami.
       </p>
       <p class="content__article-links">
         <a href="/docs/resume.pdf" target="_blank">
