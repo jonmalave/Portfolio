28d673ea34e5ec904d1fe4e28f99acaaee1457dc
Jon Malave <jonmalave@gmail.com>
2024-05-13 17:18:19 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
         Jon Malave
       </p>
       <p class="content__article-subtitle">
-        Web Developer based in Cape Coral, FL.
+        Full-Stack Web Developer
       </p>
       <p class="content__article-links">
         <a href="/docs/resume.pdf" target="_blank">
